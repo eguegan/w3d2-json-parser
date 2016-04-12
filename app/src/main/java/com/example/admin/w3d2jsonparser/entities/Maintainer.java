4abb054d35083d612da5928badf1b5855c0d5a74
eguegan <e.guegan@margulls.com>
2016-04-12 16:30:05 -0400

    Add Json Parser

---
+++ app/src/main/java/com/example/admin/w3d2jsonparser/entities/Maintainer.java
@@ -0,0 +1,31 @@
+
+package com.example.admin.w3d2jsonparser.entities;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Maintainer {
+
+    @SerializedName("github")
+    @Expose
+    private String github;
+
+    /**
+     * 
+     * @return
+     *     The github
+     */
+    public String getGithub() {
+        return github;
+    }
+
+    /**
+     * 
+     * @param github
+     *     The github
+     */
+    public void setGithub(String github) {
+        this.github = github;
+    }
+
+}
